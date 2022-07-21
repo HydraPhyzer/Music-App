ee258f9326dcb27b3e62649bc84e9e88d8499101
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-21 11:59:56 +0500

    Now We Can Navigate Next & Previous Tracks

--- Index.js
+++ Index.js
@@ -4,15 +4,18 @@ let Pause = document.querySelector('.fa-circle-pause');
 let Anime = document.querySelector('.Image img');
 let Next = document.querySelector('.Next');
 let Prev = document.querySelector('.Prev');
-let SongIndex=1;
+let MusicName=document.querySelector('.Music-Name');
+let SingerName=document.querySelector('.Singer-Name');
+let SongIndex=0;
+let isPlaying=false;
 
 Pause.style.display = "none";
 
 let Object=
 [
     {
-        SongName:"Zelda",
-        Artist:"Bujaa",
+        SongName:"Ambition For Cash",
+        Artist:"Argit Singh",
         Image:'/Logo/Logo1.jpg',
         Path:'/Songs/Music1.mp3'
     },
@@ -35,14 +38,40 @@ Play.addEventListener("click", () => {
     Play.style.display = "none";
     Anime.classList.add("Anime")
     Music.play();
+    isPlaying=true;
 })
 Pause.addEventListener("click", () => {
     Play.style.display = "block";
     Pause.style.display = "none";
     Anime.classList.remove("Anime")
     Music.pause();
+    isPlaying=false
 })
 Next.addEventListener("click" , ()=>
 {
-    alert("asas")
+    SongIndex=(SongIndex+1)%Object.length;
+    MusicName.textContent=Object[SongIndex].SongName;
+    SingerName.textContent=Object[SongIndex].Artist;
+
+    Anime.src=Object[SongIndex].Image;
+    Music.src=Object[SongIndex].Path;
+
+    if(isPlaying)
+    Music.play();
+});
+Prev.addEventListener("click" , ()=>
+{
+    SongIndex=(SongIndex-1)%Object.length;
+    if(SongIndex<0)
+    {
+        SongIndex=Object.length-1;
+    }
+    MusicName.textContent=Object[SongIndex].SongName;
+    SingerName.textContent=Object[SongIndex].Artist;
+
+    Anime.src=Object[SongIndex].Image;
+    Music.src=Object[SongIndex].Path;
+
+    if(isPlaying)
+    Music.play();
 });
